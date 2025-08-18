aa2385e912f01172aa1746e1b72259ad9e2adfc4
Arturo Rodas <arturo.rodas.gonzales@gmail.com>
2025-08-18 18:31:16 -0500

    Corrección formato stock

--- app/_utils/get-stock.tsx
+++ app/_utils/get-stock.tsx
@@ -11,17 +11,21 @@ export function getStock({
     stock_entero * (unidades_contenidas != 0 ? unidades_contenidas : 1) +
     stock_fraccion
   const unidadesCompletas =
-    unidades_contenidas != 0 ? Math.floor(stockTotal / unidades_contenidas) : 0
-  const fraccionRestante =
+    unidades_contenidas != 0 ? Math.trunc(stockTotal / unidades_contenidas) : 0
+
+  const preFraccionRestante =
     unidades_contenidas != 0 ? stockTotal % unidades_contenidas : stockTotal
+  const addNegative = preFraccionRestante < 0 && unidadesCompletas == 0
+  const fraccionRestante = Math.abs(preFraccionRestante)
 
   const fraccionFormateada =
     fraccionRestante % 1 === 0 ? fraccionRestante : fraccionRestante.toFixed(2)
 
   return {
-    stock: `${
+    stock: `${addNegative ? '-' : ''}${
       unidadesCompletas != 0 ? unidadesCompletas : ''
     }F${fraccionFormateada}`,
+    addNegative,
     unidadesCompletas,
     fraccionFormateada,
   }
@@ -38,7 +42,7 @@ export function GetStock({
   unidades_contenidas: number
   className?: string
 }) {
-  const { unidadesCompletas, fraccionFormateada } = getStock({
+  const { unidadesCompletas, fraccionFormateada, addNegative } = getStock({
     stock_entero,
     stock_fraccion,
     unidades_contenidas,
@@ -46,6 +50,7 @@ export function GetStock({
 
   return (
     <>
+      {addNegative ? '-' : ''}
       {unidadesCompletas != 0 ? unidadesCompletas : ''}
       <span className={`text-orange-600 ${className}`}>F</span>
       {fraccionFormateada}
