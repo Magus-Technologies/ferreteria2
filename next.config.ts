eac4181fceb70578d789b1b41453a964dd9b4694
Arturo Rodas <arturo.rodas.gonzales@gmail.com>
2025-08-25 19:27:23 -0500

    Test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: process.env.NODE_ENV === 'production',
+  },
 }
 
 export default nextConfig
