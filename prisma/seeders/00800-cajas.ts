1e7a95e26f772cb5e02c8c975241ff00b180480e
Arturo Rodas <arturo.rodas.gonzales@gmail.com>
2025-11-16 21:14:53 -0500

    seeders

--- prisma/seeders/00800-cajas.ts
+++ prisma/seeders/00800-cajas.ts
@@ -18,4 +18,103 @@ export default async function cajasSeeder() {
       },
     },
   })
+
+  await prisma.subCaja.create({
+    data: {
+      name: 'Caja Bancaria',
+      metodos_de_pago: {
+        create: [
+          {
+            name: 'BCP',
+            despliegue_de_pagos: {
+              create: [
+                { name: 'CB / BCP / QR' },
+                { name: 'CB / BCP / TRANSFERENCIA' },
+              ],
+            },
+          },
+          {
+            name: 'SCOTIABANK',
+            despliegue_de_pagos: {
+              create: [{ name: 'CB / SCOTIABANK / TRANSFERENCIA' }],
+            },
+          },
+          {
+            name: 'BBVA',
+            despliegue_de_pagos: {
+              create: [
+                { name: 'CB / BBVA / QR' },
+                { name: 'CB / BBVA / TRANSFERENCIA' },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  })
+
+  await prisma.subCaja.create({
+    data: {
+      name: 'Caja Ahorros',
+      metodos_de_pago: {
+        create: [
+          {
+            name: 'BCP',
+            despliegue_de_pagos: {
+              create: [{ name: 'TRANSFERENCIA', mostrar: false }],
+            },
+          },
+          {
+            name: 'SCOTIABANK',
+            despliegue_de_pagos: {
+              create: [{ name: 'TRANSFERENCIA', mostrar: false }],
+            },
+          },
+          {
+            name: 'BBVA',
+            despliegue_de_pagos: {
+              create: [{ name: 'TRANSFERENCIA', mostrar: false }],
+            },
+          },
+        ],
+      },
+    },
+  })
+
+  await prisma.subCaja.create({
+    data: {
+      name: 'Caja Negra',
+      metodos_de_pago: {
+        create: [
+          {
+            name: 'Efectivo',
+            despliegue_de_pagos: {
+              create: [{ name: 'CN / EFECTIVO' }],
+            },
+          },
+          {
+            name: 'BCP',
+            despliegue_de_pagos: {
+              create: [
+                { name: 'CN / BCP / YAPE' },
+                { name: 'CN / BCP / TRANSFERENCIA' },
+              ],
+            },
+          },
+          {
+            name: 'SCOTIABANK',
+            despliegue_de_pagos: {
+              create: [{ name: 'CN / SCOTIABANK / TRANSFERENCIA' }],
+            },
+          },
+          {
+            name: 'BBVA',
+            despliegue_de_pagos: {
+              create: [{ name: 'CN / BBVA / TRANSFERENCIA' }],
+            },
+          },
+        ],
+      },
+    },
+  })
 }
