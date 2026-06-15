f9a928dd23f407949d4f4a0bd8d39dbbab886d1e
EmerRo <yarlequerodrigo9@gmail.com>
2026-06-15 17:46:37 -0500

    sd

--- app/ui/gestion-comercial-e-inventario/mi-almacen/_components/others/cards-info.tsx
+++ app/ui/gestion-comercial-e-inventario/mi-almacen/_components/others/cards-info.tsx
@@ -2,59 +2,25 @@
 
 import CardMiniInfo from '../cards/card-mini-info'
 import { useStoreAlmacen } from '~/store/store-almacen'
-import { useStoreFiltrosProductos } from '../../_store/store-filtros-productos'
-import { useMemo } from 'react'
-import { useProductosListadoCompleto } from '../../_hooks/useProductosListadoCompleto'
+import { useQuery } from '@tanstack/react-query'
+import { inventarioReporteApi } from '~/lib/api/inventario-reporte'
 
 export default function CardsInfo() {
-  const filtros = useStoreFiltrosProductos(state => state.filtros)
   const almacen_id = useStoreAlmacen(store => store.almacen_id)
-  const almacenActual = filtros?.almacen_id || almacen_id || 1
 
-  const { data: productos } = useProductosListadoCompleto(almacenActual)
+  const { data: resumen } = useQuery({
+    queryKey: ['inventario-resumen', almacen_id],
+    queryFn: async () => {
+      const res = await inventarioReporteApi.getResumen({ almacen_id })
+      if (res.error) throw new Error(res.error.message)
+      return res.data?.data ?? null
+    },
+    staleTime: 1000 * 60 * 2,
+    enabled: !!almacen_id,
+  })
 
-  const { inversion, precio_venta } = useMemo(() => {
-    if (!Array.isArray(productos) || productos.length === 0) {
-      return { inversion: 0, precio_venta: 0 }
-    }
-
-    const inversionTotal = productos.reduce((acc, producto) => {
-      const producto_en_almacen = producto.producto_en_almacenes?.find(
-        item => item.almacen_id === almacenActual
-      )
-      return (
-        acc +
-        Number(producto_en_almacen?.costo ?? 0) *
-          Number(producto_en_almacen?.stock_fraccion ?? 0)
-      )
-    }, 0)
-
-    const precioVentaTotal = productos.reduce((acc, producto) => {
-      const producto_en_almacen = producto.producto_en_almacenes?.find(
-        item => item.almacen_id === almacenActual
-      )
-
-      const unidad_derivada =
-        producto_en_almacen?.unidades_derivadas?.find(
-          ud => Number(ud.factor) == 1
-        ) ?? producto_en_almacen?.unidades_derivadas?.[0]
-
-      const precio_publico = Number(unidad_derivada?.precio_publico ?? 0)
-      const factor = Number(unidad_derivada?.factor ?? 0)
-
-      return (
-        acc +
-        (factor == 1
-          ? precio_publico
-          : factor == 0
-          ? 0
-          : precio_publico / factor) *
-          Number(producto_en_almacen?.stock_fraccion ?? 0)
-      )
-    }, 0)
-
-    return { inversion: inversionTotal, precio_venta: precioVentaTotal }
-  }, [productos, almacenActual])
+  const inversion = resumen?.valorizacion_total ?? 0
+  const precio_venta = resumen?.valorizacion_venta ?? 0
 
   return (
     <>
